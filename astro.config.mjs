d948a533fc0e3d74942057b188311ae471f729e3
ChromaBlend Studios <153875273+ChromaBlend@users.noreply.github.com>
2026-04-22 21:30:17 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sitemap-test-em5.pages.dev', // Replaced at deploy time
+  site: 'https://sitemap-test-em5.pages.dev',
   integrations: [
     tailwind(),
-    sitemap()
+    sitemap({
+      filter: (page) => page.endsWith('/') // 👈 fix
+    })
   ],
   build: {
     format: 'directory'
